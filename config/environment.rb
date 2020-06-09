cb973f17b4958f97e24b5591cc7d22ac17849f7f
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-06-09 17:08:58 +0200

    fixed environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,15 @@
 require 'bundler'
 Bundler.require
+
+configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+end
