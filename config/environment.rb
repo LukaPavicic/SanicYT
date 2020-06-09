8e1684ff8863dfadb0e6c45546deb5e244f5d6c7
Luka Pavicic <dev.luka.pavicic@gmail.com>
2020-06-09 19:07:05 +0200

    fixing heroku

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,5 @@ require 'bundler'
 Bundler.require
 
 configure :production do
-    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
-    ActiveRecord::Base.establish_connection(
-      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-    )
+  set :database, ENV['DATABASE_URL']
 end
